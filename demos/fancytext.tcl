ff83ff9e454ec39ef2511a4bef04596d921443bc
Andy Bennett <andyjpb@ashurst.eu.org>
2023-08-11 16:24:18 +0100

    Fix reference to `wish` executable in demo scripts
    I have `wish` and `wish8.6` executables.
    
    8.X is available almost everywhere these days so assume that `wish` gives
    us an 8.X `wish` so that we don't have to change this again for every minor
    version.
    
    Signed-off-by: Andy Bennett <andyjpb@ashurst.eu.org>

--- demos/fancytext.tcl
+++ demos/fancytext.tcl
@@ -32,7 +32,7 @@
 # $Id: demo.tcl,v 1.1 1997/09/17 21:10:23 donal Exp donal $
 
 # Now we make cunning use of the backslash/shell trick \
-[ -x `dirname $0`/../shapewish ] && exec `dirname $0`/../shapewish $0 ${1+"$@"} || exec wish8.0 $0 ${1+"$@"} || { echo "`basename $0`: couldn't start wish" >&2 ; exit 1; }
+[ -x `dirname $0`/../shapewish ] && exec `dirname $0`/../shapewish $0 ${1+"$@"} || exec wish $0 ${1+"$@"} || { echo "`basename $0`: couldn't start wish" >&2 ; exit 1; }
 
 set dir [file join [pwd] [file dirname [info script]] ..]
 lappend auto_path [file join $dir ..]
