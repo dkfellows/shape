0bc04d1ae8a133d83ab7887f13cbe49aa4079ee7
Christopher Chavez <chrischavez@gmx.us>
2023-11-13 04:44:03 -0600

    shape.c: fix compilation
    shape.c:404:47: error: cannot take the address of an rvalue of type 'void *'
      404 |     imageName = Tcl_GetStringFromObj(objv[0], &NULL);
          |                                               ^~~~~

--- generic/shape.c
+++ generic/shape.c
@@ -401,7 +401,7 @@ shapePhoto(
 	return TCL_ERROR;
     }
 
-    imageName = Tcl_GetStringFromObj(objv[0], &NULL);
+    imageName = Tcl_GetString(objv[0]);
     handle = Tk_FindPhoto(interp, imageName);
     if (handle == NULL) {
 	return TCL_ERROR;
